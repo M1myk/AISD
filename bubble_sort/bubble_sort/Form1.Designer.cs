f8160301b985eb053aad97a355a6e71af1c999d7
M1myk <147138111+M1myk@users.noreply.github.com>
2025-01-04 12:51:04 +0100

    Add files via upload

---
+++ bubble_sort/bubble_sort/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace bubble_sort
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label3 = new Label();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(327, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(333, 27);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(327, 181);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(333, 27);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(77, 291);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 40);
+            button1.TabIndex = 2;
+            button1.Text = "BubbleSort";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(18, 181);
+            label1.Name = "label1";
+            label1.Size = new Size(235, 26);
+            label1.TabIndex = 3;
+            label1.Text = "Otrzymane liczby:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(24, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(181, 26);
+            label2.TabIndex = 4;
+            label2.Text = "Wpisac liczby:";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(214, 353);
+            button2.Name = "button2";
+            button2.Size = new Size(148, 40);
+            button2.TabIndex = 5;
+            button2.Text = "MergeSort";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button3.Location = new Point(277, 291);
+            button3.Name = "button3";
+            button3.Size = new Size(145, 40);
+            button3.TabIndex = 6;
+            button3.Text = "QuickSort";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button4.Location = new Point(394, 353);
+            button4.Name = "button4";
+            button4.Size = new Size(189, 40);
+            button4.TabIndex = 7;
+            button4.Text = "SelectionSort";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button5.Location = new Point(450, 291);
+            button5.Name = "button5";
+            button5.Size = new Size(193, 41);
+            button5.TabIndex = 8;
+            button5.Text = "InsertionSort";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            numericUpDown1.Location = new Point(256, 87);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(58, 33);
+            numericUpDown1.TabIndex = 9;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(24, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(226, 26);
+            label3.TabIndex = 10;
+            label3.Text = "Generowac liczby";
+            // 
+            // button6
+            // 
+            button6.Font = new Font("RomanT", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button6.Location = new Point(631, 353);
+            button6.Name = "button6";
+            button6.Size = new Size(145, 40);
+            button6.TabIndex = 11;
+            button6.Text = "QuickSort";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private Button button6;
+    }
+}
---
+++ bubble_sort/bubble_sort/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+namespace bubble_sort
+
+    
+    // dodac counting sort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nierozdzielone = textBox1.Text;
+            string[] liczby = nierozdzielone.Split(',', ' ');
+            int[] lista = new int[liczby.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = int.Parse(liczby[i]);
+            }
+            //bubble_sort(lista);
+
+            // Wywołanie funkcji bubble_sort i otrzymanie posortowanej listy
+            bubble_sort(lista);
+
+            // Wyświetlenie wyniku w textBox2
+            string wyjscie = string.Join(", ", lista);
+            textBox2.Text = wyjscie;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nierozdzielone = textBox1.Text;
+            string[] liczby = nierozdzielone.Split(',', ' ');
+            int[] lista = new int[liczby.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = int.Parse(liczby[i]);
+            }
+            merge_sort(lista);
+
+            string wyjscie = string.Join(", ", lista);
+            textBox2.Text = wyjscie;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nierozdzielone = textBox1.Text;
+            string[] liczby = nierozdzielone.Split(',', ' ');
+            int[] lista = new int[liczby.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = int.Parse(liczby[i]);
+            }
+            quick_sort(lista, 0, lista.Length - 1);
+
+            string wyjscie = string.Join(", ", lista);
+            textBox2.Text = wyjscie;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string nierozdzielone = textBox1.Text;
+            string[] liczby = nierozdzielone.Split(',', ' ');
+            int[] lista = new int[liczby.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = int.Parse(liczby[i]);
+            }
+            selection_sort(lista);
+
+            string wyjscie = string.Join(", ", lista);
+            textBox2.Text = wyjscie;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string nierozdzielone = textBox1.Text;
+            string[] liczby = nierozdzielone.Split(',', ' ');
+            int[] lista = new int[liczby.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = int.Parse(liczby[i]);
+            }
+            List<int> nowa_lista =  insertion_sort(lista);
+
+            string wyjscie = string.Join(", ", nowa_lista);
+            textBox2.Text = wyjscie;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            string nierozdzielone = textBox1.Text;
+            string[] liczby = nierozdzielone.Split(',', ' ');
+            int[] lista = new int[liczby.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = int.Parse(liczby[i]);
+            }
+            List<int> new_lista =  counting_sort(lista);
+
+            string wyjscie = string.Join(", ", new_lista);
+            textBox2.Text = wyjscie;
+        }
+
+        private void bubble_sort(int[] lista)
+        {
+            Boolean zamienono = true;
+
+            while (zamienono)
+            {
+                zamienono = false;
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] > lista[i + 1])
+                    {
+                        int temp = lista[i];
+                        lista[i] = lista[i + 1];
+                        lista[i + 1] = temp;
+
+                        zamienono = true;
+                    }
+
+
+                }
+            }
+            
+
+        }
+
+        private void merge_sort(int[] lista)
+        {
+            if(lista.Length > 1)
+            {
+                int mid = lista.Length / 2;
+                int[] left = new int[mid];
+                int[] right = new int[lista.Length-mid];
+
+                Array.Copy(lista, 0, left, 0, mid);
+                Array.Copy(lista, mid, right, 0, lista.Length-mid);
+
+                merge_sort(left);
+                merge_sort(right);
+
+
+                int i = 0, j = 0, k = 0;
+
+                while(i < left.Length && j < right.Length)
+                {
+                    if(left[i] < right[j])
+                    {
+                        lista[k] = left[i];
+                        i++;
+                    }
+                    else
+                    {
+                        lista[k] = right[j];
+                        j++;
+                    }
+                    k++;
+                }
+
+                while(i < left.Length)
+                {
+                    lista[k] = left[i];
+                    i++;
+                    k++;
+                }
+                while (j < right.Length)
+                {
+                    lista[k] = right[j];
+                    j++;
+                    k++;
+                }
+            }
+        }
+
+
+        private void quick_sort(int[] lista, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = lista[right];
+                int i = left - 1;
+                for(int j = left; j < right; j++)
+                {
+                    if(lista[j] < pivot)
+                    {
+                        i += 1;
+
+                        int temp = lista[i];
+                        lista[i] = lista[j];
+                        lista[j] = temp;
+                    }
+
+                }
+                int temp1 = lista[i + 1];
+                lista[i + 1] = lista[right];
+                lista[right] = temp1;
+
+
+                int index = i + 1;
+                quick_sort(lista, left, index - 1);
+                quick_sort(lista, index + 1, right);
+
+            }
+
+
+        }
+
+        private void selection_sort(int[] lista)
+        {
+           for(int i = 0; i < lista.Length; i++)
+            {
+                int min = i;
+                for(int j = i + 1; j < lista.Length; j++)
+                {
+                    if( lista[j] < lista[min] )
+                    {
+                        min = j;
+
+                        
+                       
+                    }
+                }
+                int temp = lista[i];
+                lista[i] = lista[min];
+                lista[min] = temp;
+            }
+        }
+
+        private List<int> insertion_sort(int[] lista)
+        {
+            for(int i = 1;i < lista.Length;i++)
+            {
+                int key = lista[i];
+                int j = i - 1;
+                while(j >= 0 && lista[j] > key)
+                {
+                    lista[j + 1] = lista[j];
+                    j--;
+                }
+                lista[j + 1] = key;
+            }
+
+            List<int> insertion_list = new List<int>();
+
+            for(int i = 0;i < lista.Length;i++)
+            {
+                insertion_list.Add(lista[i]);
+            }
+            return insertion_list;
+        }
+
+        public List<int> counting_sort(int[] lista)
+        {
+            int maxVal = lista.Max();
+            int[] count = new int[maxVal+1];
+
+            foreach(int num in lista)
+            {
+                count[num]++;
+            }
+
+            List<int> list = new List<int>();
+            for(int i = 0;  i < count.Length; i++)
+            {
+                while(count[i] > 0)
+                {
+                    list.Add(i);
+                    count[i]--;
+                }
+            }
+            return list;
+               
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int numberOfElements = (int)numericUpDown1.Value;
+            int[] numbers = generowanie_liczb(numberOfElements);
+
+
+            textBox1.Text = string.Join(" ", numbers);
+        }
+
+        private int[] generowanie_liczb(int count)
+        {
+            Random random = new Random();
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+            }
+
+            return numbers;
+        }
+
+        
+    }
+}
